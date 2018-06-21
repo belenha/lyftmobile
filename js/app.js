1197dd78c2271d4438bb541095111c2b41135e1e
belenha <lubinatica@gmail.com>
2018-06-21 11:52:13 -0500

    splash screen

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
+// function splash(param) {
+//   var time = param;
+//   setTimeout(function () {
+//     $('#splashscreen').hide();
+//   }, time);
+//  }
+
+// $(function() {
+//   $("#splashScreen").show().delay(3000).hide();
+// });
+
+document.addEventListener("DOMContentLoaded", function(){
+  $('#splashScreen').delay(2000).fadeOut('slow');
+  
+  $('.preloader-wrapper')
+      .delay(1000)
+      .fadeOut('slow');
+});
+
 $(document).ready(function(){
     $('select').formSelect();
   });
