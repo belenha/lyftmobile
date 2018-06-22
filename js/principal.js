2859c938e6af10a6372be697ac16728480dc2360
belenha <lubinatica@gmail.com>
2018-06-22 12:09:02 -0500

    código de verificación

--- js/principal.js
+++ js/principal.js
@@ -1,21 +1,11 @@
-function rand_code(chars, lon){
-  code = "";
-  for (x=0; x < lon; x++)
-  {
-  rand = Math.floor(Math.random()*chars.length);
-  code += chars.substr(rand, 1);
-  }
-  return code;
-  }
-  
-  caracteres = "0123456789";
-  longitud = 3;
-  
-  alert("LAB-" + (rand_code(caracteres, longitud)));
+
+ 
+ 
+ // ***** DESACTIVAR BOTON NEXT & CODIGO *****
 
 $("#contact-phone").keypress(function () {
   var value = $("#contact-phone").val().length;
-  // console.log(valueT);
+
   if (value === '' || value === 9) {
       $("#next").prop("disabled", false);
 
@@ -30,39 +20,25 @@ $("#contact-phone").keypress(function () {
       $("#contact-phone").prop("disabled", true);
   }
 
+ var $randomNumber = 0;
+ $randomNumber = parseInt(Math.random() *1000);
+ 
+ $('#next').click(function(){
+ swal("Tu codigo de verificación es : LAB- " +  $randomNumber);
+ });
+
 });
+
+// ***** SELECT *****
 
 $(document).ready(function(){
     $('select').formSelect();
   });
-
+// ***** MODAL *****
 
 $(document).ready(function(){
     $('.modal').modal();
     $('#next').click();
   });
 
- 
 
-
-
-
-  //   function getContactData (){
-//     var name = $('#contact-name').val();
-    // var phone = $('#contact-phone').val();
-    // var phone = ('#contact-phone').val();
-
-
-    
-    // console.log(name, $phone);
-    // addContactData(name, $phone);
-
-    // $('#contact-name').val('');
-    // $('#contact-phone').val('');
-    // $phone.val=('');
-
-// }
-//     $('main').append(finalTemplate);
-//     $('h5').html('Contactos: ' + count);
-//     swal("Good job!", "Ya tienes otro amigo", "success");
-// }
